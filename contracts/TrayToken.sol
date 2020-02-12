3dc2b02ad73ac2e45229c8664a973c0cf5af9063
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-12 16:31:57 +0530

    Symbol check done

--- contracts/TrayToken.sol
+++ contracts/TrayToken.sol
@@ -4,6 +4,8 @@ contract TrayToken {
   // Setting State Variables, accessable through out the contract
   // we get getter function for free for public state variable
   uint256 public totalSupply;
+  string public name = 'TrayToken';
+  string public symbol = 'TR';
   // Tracks the acoounts and how much token it holds in key-value pair
   mapping(address => uint256) public balanceOf;
 
--- test/tray_token.js
+++ test/tray_token.js
@@ -4,6 +4,18 @@ const TrayToken = artifacts.require("TrayToken");
 contract("TrayToken", function(accounts) {
   var token;
 
+  it("should initialize contract with correct values", async function() {
+    return TrayToken.deployed().then(function(instance) {
+      token = instance;
+      return token.name();
+    }).then(function(token_name) {
+      assert.equal(token_name, 'TrayToken', 'Token name check');
+      return token.symbol();
+    }).then(function(token_symbol) { 
+      assert.equal(token_symbol, 'TR', 'Token symbol check');
+    });
+  })
+
   it("should set total supply upon deployment", async function() {
     return TrayToken.deployed()
       .then(function(instance) { 
@@ -11,11 +23,18 @@ contract("TrayToken", function(accounts) {
         return token.totalSupply()
       }).then(function(total_supply) {
         assert.equal(total_supply, 1000, 'Sets totalSupply to 1000 tokens');
-	return token.balanceOf(accounts[0]);
-      }).then(function(admin_tokens) {
-	assert.equal(admin_tokens, 1000, 'Transfer tokens to admin account');
-         });
-  }); 
+      });
+  })
+
+
+  it("Should transfer minted tokens to admin account", async function() {
+    return TrayToken.deployed().then(function(instance) {
+      token = instance;
+      return token.balanceOf(accounts[0]);
+    }).then(function(admin_tokens) {
+      assert.equal(admin_tokens, 1000, 'Tokens transfered to admin account');
+    });
+  });
 
 
 })
