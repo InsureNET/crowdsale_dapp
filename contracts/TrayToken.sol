a1f8d95665ac4dca5df17332ecfa478c34f30729
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-12 19:19:09 +0530

    Transfer function coded and tested

--- contracts/TrayToken.sol
+++ contracts/TrayToken.sol
@@ -9,12 +9,29 @@ contract TrayToken {
   // Tracks the acoounts and how much token it holds in key-value pair
   mapping(address => uint256) public balanceOf;
 
+  event Transfer(
+	  address indexed _from,
+	  address indexed _to,
+	  uint256 _value);
+
 
   constructor(uint256 _initialSupply) public {
     // Total number of token
     totalSupply = _initialSupply;
     // Setting all minted tokens to admin account
     balanceOf[msg.sender] = _initialSupply;
-
   }
+
+ 
+  function transfer(address _to, uint256 _value) public returns(bool success) {
+    // Checking sender have enough tokens
+    require(balanceOf[msg.sender] >= _value);
+    // reducing and incrementing balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
 }
--- test/tray_token.js
+++ test/tray_token.js
@@ -16,6 +16,7 @@ contract("TrayToken", function(accounts) {
     });
   })
 
+
   it("should set total supply upon deployment", async function() {
     return TrayToken.deployed()
       .then(function(instance) { 
@@ -34,7 +35,31 @@ contract("TrayToken", function(accounts) {
     }).then(function(admin_tokens) {
       assert.equal(admin_tokens, 1000, 'Tokens transfered to admin account');
     });
-  });
+  })
+
+
+  it("Should transfer token and there must reduction in token", async function() {
+    return TrayToken.deployed().then(function(instance) {
+      token = instance;
+      return token.transfer.call(accounts[1], 500, { from: accounts[0] });
+    }).then(function(return_value) {
+      assert.equal(return_value, true, 'Return value from transfer function check');
+      return token.transfer(accounts[1], 500, { from: accounts[0] });
+    }).then(function(receipt) {
+      // Testing Transfer Event
+      assert.equal(receipt.logs.length, 1, 'One event triggered');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'Confirmed the event is TRANSFER');
+      assert.equal(receipt.logs[0].args._from, accounts[0], 'Sender account check');
+      assert.equal(receipt.logs[0].args._to, accounts[1], 'Recepient account check');
+      assert.equal(receipt.logs[0].args._value, 500, 'Amount to transfer check');	
+      return token.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance, 500, 'Token received by the recepient');
+      return token.balanceOf(accounts[0]);
+    }).then(function(balance) { 
+      assert.equal(balance, 500, 'Token deducted from the sender');
+    });
+  }); 
 
 
 })
