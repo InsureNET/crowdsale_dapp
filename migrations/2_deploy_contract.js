f781be587ef6f2b8fecfc95856808409a67731f8
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-12 22:34:33 +0530

    Migration file update

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,7 @@
 const TrayToken = artifacts.require("TrayToken");
+const CrowdSale = artifacts.require("CrowdSale");
 
 module.exports = function(deployer) {
   deployer.deploy(TrayToken, 1000);
+  deployer.deploy(CrowdSale);
 };
