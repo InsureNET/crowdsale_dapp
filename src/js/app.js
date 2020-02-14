e38e42ec564edf3df7c48af34641102ca0ce9112
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-14 15:16:15 +0530

    App.js initialized

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-console.log("App js file working")
+App = {
+  web3Provider: null,
+
+  init: function() {
+    console.log("Initialization Done")
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if(typeof web3 !== 'undefined') {
+      // web3 instance is provided by MetaMask
+      web3 = new Web3(App.web3Provider);
+    } else {
+      // default instance of web3
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+      web3 = new Web3(App.web3Provider);
+    }
+  }
+}
+
+
+
+
+// To call the app initialization function
+$(function() {
+  $(window).ready(function() {
+    App.init();
+  })
+});
