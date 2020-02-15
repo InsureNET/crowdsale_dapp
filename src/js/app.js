dfac23045e462ee46e7abd4f5303daaea64b5433
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-15 10:11:21 +0530

    Older version Web3 js file added and working fine

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,11 @@
 App = {
   web3Provider: null,
+  contracts: {},
 
   init: function() {
     console.log("Initialization Done")
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
-    if(typeof web3 !== 'undefined') {
-      // web3 instance is provided by MetaMask
-      web3 = new Web3(App.web3Provider);
-    } else {
-      // default instance of web3
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-      web3 = new Web3(App.web3Provider);
-    }
   }
+
 }
 
 
