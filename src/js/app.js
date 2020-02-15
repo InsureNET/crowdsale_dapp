1716e09904a75318aa1d5c11db540df12297ffd6
XPS <5089212-xpsdemon@users.noreply.gitlab.com>
2020-02-15 10:49:38 +0530

    Web3 instance successfully get from Metamask

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,20 @@ App = {
 
   init: function() {
     console.log("Initialization Done")
+    App.initWeb3();
+  },
+
+  
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If web3 instance is already provided by MetaMask
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      // Create special instance if web3 is not provided
+      App.web3Provider = new Web3.providers.HttpProvider('http:localhost:8545');
+      web3 = new Web3(App.web3Provider)
+    }
   }
 
 }
