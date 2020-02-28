3eb9dc7ae0bfba9a0b5aee561f6a25c6e1cd6edb
Jaxcoder <codenamejason@outlook.com>
2020-02-28 09:08:26 -0500

    renaming

--- contracts/USFToken.sol
+++ contracts/USFToken.sol
@@ -1,72 +1,87 @@
-pragma solidity >= 0.5.0 < 0.7.0;
+pragma solidity ^0.5.0;
 
-contract USFToken {
-  // Setting State Variables, accessable through out the contract
-  // we get getter function for free for public state variable
-  uint256 public totalSupply;
-  string public name = 'USFToken';
-  string public symbol = 'USF';
+/* Defining token attributes */
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+import "../openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
+import "../openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-  // Tracks the acoounts and how much token it holds in key-value pair
-  mapping(address => uint256) public balanceOf;
+contract USFToken is ERC20Mintable, ERC20Pausable, ERC20Detailed {
 
-  // Hold howmuch is permitted to transfer for B on A's behalf
-  mapping(address => mapping(address => uint256)) public allowance;
-
-  event Transfer(
-	  address indexed _from,
-	  address indexed _to,
-	  uint256 _value);
-
-  event Approval(
-	  address indexed _owner,
-	  address indexed _spender,
-	  uint256 _value);
-
-  constructor(uint256 _initialSupply) public {
-    // Total number of token
-    totalSupply = _initialSupply;
-    // Setting all minted tokens to admin account
-    balanceOf[msg.sender] = _initialSupply;
-  }
-
-  function transfer(address _to, uint256 _value) public returns(bool success) {
-    // Checking sender have enough tokens
-    require(balanceOf[msg.sender] >= _value, "");
-    // reducing and incrementing balance
-    balanceOf[msg.sender] -= _value;
-    balanceOf[_to] += _value;
-
-    emit Transfer(msg.sender, _to, _value);
-
-    return true;
-  }
-
-  function approve(address _spender, uint256 _value) public returns(bool success) {
-  // Account A allowing account B to transfer _value tokens on A's suggestion
-    allowance[msg.sender][_spender] = _value;
-
-    emit Approval(msg.sender, _spender, _value);
-
-    return true;
-  }
-
-  // This function is called if sender dont initiate the transfer to transfer token to recepient
-  function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
-    // Condition check
-    require(balanceOf[_from] >= _value, "");
-    require(allowance[_from][msg.sender] >= _value, "");
-
-    // Incrementing and decrementing value
-    balanceOf[_from] -= _value;
-    balanceOf[_to] += _value;
-
-    // Update allowance
-    allowance[_from][msg.sender] -= _value;
-
-    emit Transfer(_from, _to, _value);
-
-    return true;
-  }
-
+  constructor(string memory name, string memory symbol, uint8 decimals)
+     ERC20Detailed(name, symbol, decimals)
+     public {}
 }
+
+//
+
+// contract USFToken {
+//   // Setting State Variables, accessable through out the contract
+//   // we get getter function for free for public state variable
+//   uint256 public totalSupply;
+//   string public name = 'USFToken';
+//   string public symbol = 'USF';
+
+//   // Tracks the acoounts and how much token it holds in key-value pair
+//   mapping(address => uint256) public balanceOf;
+
+//   // Hold howmuch is permitted to transfer for B on A's behalf
+//   mapping(address => mapping(address => uint256)) public allowance;
+
+//   event Transfer(
+// 	  address indexed _from,
+// 	  address indexed _to,
+// 	  uint256 _value);
+
+//   event Approval(
+// 	  address indexed _owner,
+// 	  address indexed _spender,
+// 	  uint256 _value);
+
+//   constructor(uint256 _initialSupply) public {
+//     // Total number of token
+//     totalSupply = _initialSupply;
+//     // Setting all minted tokens to admin account
+//     balanceOf[msg.sender] = _initialSupply;
+//   }
+
+//   function transfer(address _to, uint256 _value) public returns(bool success) {
+//     // Checking sender have enough tokens
+//     require(balanceOf[msg.sender] >= _value, "");
+//     // reducing and incrementing balance
+//     balanceOf[msg.sender] -= _value;
+//     balanceOf[_to] += _value;
+
+//     emit Transfer(msg.sender, _to, _value);
+
+//     return true;
+//   }
+
+//   function approve(address _spender, uint256 _value) public returns(bool success) {
+//   // Account A allowing account B to transfer _value tokens on A's suggestion
+//     allowance[msg.sender][_spender] = _value;
+
+//     emit Approval(msg.sender, _spender, _value);
+
+//     return true;
+//   }
+
+//   // This function is called if sender dont initiate the transfer to transfer token to recepient
+//   function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+//     // Condition check
+//     require(balanceOf[_from] >= _value, "");
+//     require(allowance[_from][msg.sender] >= _value, "");
+
+//     // Incrementing and decrementing value
+//     balanceOf[_from] -= _value;
+//     balanceOf[_to] += _value;
+
+//     // Update allowance
+//     allowance[_from][msg.sender] -= _value;
+
+//     emit Transfer(_from, _to, _value);
+
+//     return true;
+//   }
+
+// }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,10 +1,19 @@
 const USFToken = artifacts.require("USFToken");
-const CrowdSale = artifacts.require("CrowdSale");
+const USFTokenCrowdsale = artifacts.require("USFTokenCrowdsale");
+
+/* Token Parameter */ 
+const name = "US Forestry Token";
+const symbol = "USF";
+const decimals = 18;
+
 
 module.exports = function(deployer) {
-  deployer.deploy(USFToken, 1000000000).then(function(instance) {
+  /* Passing constructor value */
+  // then(deployer.deploy());
+  deployer.deploy(USFToken, name, symbol, decimals).then(function(instance) {
     // Token price in WEI wihich equals 0.01 Ether
     token_price = 100000000000000;
-    return deployer.deploy(CrowdSale, instance.address, token_price);
+    return deployer.deploy(USFTokenCrowdsale, instance.address, token_price);
   })
 };
+
